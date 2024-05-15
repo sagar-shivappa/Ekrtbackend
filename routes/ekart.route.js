4c130ccb2ce83ec55e4f7f03bae1c55fb697bec2
sagar-shivappa <shivappasagar@gmail.com>
2024-05-15 13:34:01 +0530

    Stubs created 1

--- routes/ekart.route.js
+++ routes/ekart.route.js
@@ -11,18 +11,13 @@ const {
 const router = express.Router();
 
 //Add route to Post user login credenetails
-router.post("/login", postLoginUser);
 
 //Add route to Get all products
-router.get("/products", getAllProducts);
 
 //Add route to Get a user cart items by userid
-router.get("/cart/:user_id", getCartItemsByUserId);
 
 //Add route to Add products to the cart
-router.post("/cart", addToCart);
 
 //Add route to Delete a product by id
-router.delete("/cart/:user_id/:product_id", removeFromCart);
 
 module.exports = router;
